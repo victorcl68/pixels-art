d753e96946a11888bc3112f59cd7a722beeb25de
Victor Cabrera Lopes Cardoso <victor.cabrera0209@gmail.com>
2021-03-17 09:52:15 -0300

    Algumas correções do código

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+const firstTr = document.createElement('tr');
+const firstTable = document.querySelector('#color-palette');
+firstTable.appendChild(firstTr);
+for (let i = 0; i < 4; i += 1) {
+  const firstTd = document.createElement('td');
+  firstTd.className = 'color';
+  firstTr.appendChild(firstTd);
+}
+
+document.querySelector('.color').className += ' selected';
+
+for (let i = 0; i < 5; i += 1) {
+  const newTr = document.createElement('tr');
+  const theTable = document.querySelector('#pixel-board');
+  theTable.appendChild(newTr);
+  for (let i = 0; i < 5; i += 1) {
+    const newTd = document.createElement('td');
+    newTd.className = 'pixel';
+    newTr.appendChild(newTd);
+  }
+}
+
 const corPaleta = document.querySelectorAll('.color');
 
 function selectedColor(event) {
@@ -14,3 +36,18 @@ function firstFunction() {
 }
 
 firstFunction();
+
+const pixels = document.querySelectorAll('.pixel');
+
+function selectedPixel(event) {
+  const evento = event;
+  evento.target.className = 'pixel pselected';
+}
+
+function secondFunction() {
+  for (let i = 0; i < pixels.length; i += 1) {
+    pixels[i].addEventListener('click', selectedPixel);
+  }
+}
+
+secondFunction();
