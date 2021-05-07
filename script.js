2d0ada967ff92b281214e03a85e53ca0e66fd664
Victor Lopes <victor.cabrera0209@gmail.com>
2021-05-07 01:09:08 -0300

    Solving general issues

--- script.js
+++ script.js
@@ -1,38 +1,45 @@
-// Refatorar todo código
-const creationFirstTr = document.createElement('tr');
-const creationPalette = document.querySelector('#color-palette');
-creationPalette.appendChild(creationFirstTr);
-for (let index = 0; index < 4; index += 1) {
-  const creationFirstTd = document.createElement('td');
-  creationFirstTd.className = 'color';
-  creationFirstTr.appendChild(creationFirstTd);
-}
-
-document.querySelector('.color').className += ' selected';
-
-const pixelBoard = document.querySelector('#pixel-board');
-
-for (let index = 0; index < 5; index += 1) {
-  const createTr = document.createElement('tr');
-  createTr.className = 'pixeltr';
-  pixelBoard.appendChild(createTr);
-  for (let count = 0; count < 5; count += 1) {
-    const createSecondTd = document.createElement('td');
-    createSecondTd.className = 'pixel';
-    createTr.appendChild(createSecondTd);
+const paleteCreation = () => {
+  const colorPalete = document.querySelector('#color-palette');
+  const tr = document.createElement('tr');
+  colorPalete.appendChild(tr);
+  for (let index = 0; index < 4; index += 1) {
+    const td = document.createElement('td');
+    td.className = 'color';
+    tr.appendChild(td);
   }
-}
-
-const corPaleta = document.querySelectorAll('.color');
-
-const selectedColor = (event) => {
-  const corEscolhida = document.querySelector('.selected');
-  corEscolhida.className = 'color';
-  const evento = event;
-  evento.target.className += ' selected';
 };
 
-const rgbColors = () => {
+const setSelectedColorToBlack = () => {
+  document.querySelectorAll('.color')[0].className += ' selected';
+};
+
+const pixelBoardCreation = () => {
+  const boardSize = 5; // const for now
+  const pixelBoard = document.querySelector('#pixel-board');
+  for (let index = 0; index < boardSize; index += 1) {
+    const tr = document.createElement('tr');
+    pixelBoard.appendChild(tr);
+    for (let count = 0; count < boardSize; count += 1) {
+      const td = document.createElement('td');
+      td.className = 'pixel';
+      tr.appendChild(td);
+    }
+  }
+};
+
+const setingEventListenersToPalette = () => {
+  const paleta = document.querySelectorAll('.color');
+  for (let index = 0; index < paleta.length; index += 1) {
+    paleta[index].addEventListener('click', (event) => {
+      const corEscolhida = document.querySelector('.selected');
+      corEscolhida.className = 'color';
+      const evento = event;
+      evento.target.className += ' selected';
+    });
+  }
+};
+
+const RGBColors = () => {
   const r = Math.ceil(Math.random() * 255);
   const g = Math.ceil(Math.random() * 255);
   const b = Math.ceil(Math.random() * 255);
@@ -41,67 +48,76 @@ const rgbColors = () => {
 
 const corAleatoria = () => {
   for (let i = 1; i < 4; i += 1) {
-    document.getElementsByClassName('color')[i].style.backgroundColor = rgbColors();
+    document.getElementsByClassName('color')[i].style.backgroundColor = RGBColors();
   }
 };
-corAleatoria();
 
-const selectedPixel = (event) => {
-  const evento = event;
-  evento.target.className = 'pixel pselected';
-  const colorSelected = document.querySelector('.selected');
-  const style = getComputedStyle(colorSelected);
-  const bgColorSelected = style.backgroundColor;
-  evento.target.style.backgroundColor = bgColorSelected;
+const addEventClickTds = () => {
+  const pixels = document.querySelectorAll('.pixel');
+  for (let index = 0; index < pixels.length; index += 1) {
+    pixels[index].addEventListener('click', (event) => {
+      const evento = event;
+      evento.target.className = 'pixel pselected';
+      const colorSelected = document.querySelector('.selected');
+      const style = getComputedStyle(colorSelected);
+      const bgColorSelected = style.backgroundColor;
+      evento.target.style.backgroundColor = bgColorSelected;
+    });
+  }
 };
 /* A função acima do código que é para resolução do requisito 8 só consegui completar
 por causa da ajuda do Lucas Martins da Silva e do Matheus Henrique de Gois */
 
-for (let index = 0; index < corPaleta.length; index += 1) {
-  corPaleta[index].addEventListener('click', selectedColor);
-}
-
-const pixels = document.querySelectorAll('.pixel');
-
-for (let index = 0; index < pixels.length; index += 1) {
-  pixels[index].addEventListener('click', selectedPixel);
-}
-
-const clearButton = document.querySelector('#clear-board');
-const allPixel = document.querySelectorAll('.pixel');
-
-clearButton.addEventListener('click', () => {
-  for (let index = 0; index < allPixel.length; index += 1) {
-    allPixel[index].style.backgroundColor = 'white';
-  }
-});
+const createClearButton = () => {
+  const clearButton = document.querySelector('#clear-board');
+  clearButton.addEventListener('click', () => {
+    const allPixel = document.querySelectorAll('.pixel');
+    for (let index = 0; index < allPixel.length; index += 1) {
+      allPixel[index].style.backgroundColor = 'white';
+    }
+  });
+};
 
 const generateNewBoard = (param) => {
+  const pixelBoard = document.querySelector('#pixel-board');
   pixelBoard.innerHTML = '';
-  let theParam = param;
-  if (theParam < 5) {
-    theParam = 5;
-  } else if (theParam > 50) {
-    theParam = 50;
+  let myParam = param;
+  if (myParam < 5) {
+    myParam = 5;
+  } if (myParam > 50) {
+    myParam = 50;
   }
-  for (let index = 0; index < theParam; index += 1) {
-    const createTr = document.createElement('tr');
-    createTr.className = 'pixeltr';
-    pixelBoard.appendChild(createTr);
-    for (let count = 0; count < theParam; count += 1) {
-      const createSecondTd = document.createElement('td');
-      createSecondTd.className = 'pixel';
-      createTr.appendChild(createSecondTd);
+  for (let index = 0; index < myParam; index += 1) {
+    const tr = document.createElement('tr');
+    pixelBoard.appendChild(tr);
+    for (let count = 0; count < myParam; count += 1) {
+      const td = document.createElement('td');
+      td.className = 'pixel';
+      tr.appendChild(td);
     }
   }
 };
 
-const buttonBoardSize = document.querySelector('#generate-board');
-const inputBoardSize = document.querySelector('#board-size');
-console.log(inputBoardSize.value);
-buttonBoardSize.addEventListener('click', () => {
-  if (inputBoardSize.value.length === 0) {
-    alert('Board inválido!');
-  }
-  generateNewBoard(inputBoardSize.value);
-});
+const buttonVQVNewBoard = () => {
+  const generateBoard = document.querySelector('#generate-board');
+  const boardSize = document.querySelector('#board-size');
+  generateBoard.addEventListener('click', () => {
+    if (boardSize.value.length === 0) {
+      alert('Board inválido!');
+    }
+    generateNewBoard(boardSize.value);
+    addEventClickTds();
+  });
+};
+
+window.onload = () => {
+  paleteCreation();
+  setSelectedColorToBlack();
+  pixelBoardCreation();
+  setingEventListenersToPalette();
+  RGBColors();
+  corAleatoria();
+  addEventClickTds();
+  createClearButton();
+  buttonVQVNewBoard();
+};
